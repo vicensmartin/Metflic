da3baee7e771bc90ce0617f1f455964a3c38dd01
vicensmartin <131385268+vicensmartin@users.noreply.github.com>
2023-06-23 20:33:01 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,6 +3,26 @@ const CACHE = "version1";
 importScripts(
 "https://storage.googleapis.com/workbox-cdn/releases/7.0.0/workbox-sw.js");
 
+const precachedAssets = [
+  '/index.html',
+  '/favoritos.html',
+  '/trailer.html',
+  '/images/icono.png',
+  '/js/index.js',
+  '/js/favoritos.js',
+  '/js/trailer.js',
+  '/css/main.css',
+  '/css/favoritos.css',
+];
+
+
+self.addEventListener('install', (event) => {
+
+  event.waitUntil(caches.open(CACHE).then((cache) => {
+    return cache.addAll(precachedAssets);
+  }));
+});
+
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
     self.skipWaiting();
